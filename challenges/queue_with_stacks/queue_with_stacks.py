65ada6d35f29b8eb3f6e76ac2fcf4e3fe5bea317
Brandon Haynes <brandon@Brandons-MBP.localdomain>
2018-08-27 15:13:38 -0700

    initial coding for code challenge 11 queue_with_stacks

---
+++ challenges/queue_with_stacks/queue_with_stacks.py
@@ -0,0 +1,15 @@
+class Queue(object):
+    def __init__(self):
+        self.stack_one = []
+        self.stack_two = []
+
+    def enqueue(self, val):
+        for i in stack_two:
+            stack_one.push(stack_two.pop())
+        return stack_one.push(val)
+
+    def dequeue(self):
+        for i in stack_one:
+            stack_two.push(stack_one.pop())
+        return stack_two.pop()
+        
